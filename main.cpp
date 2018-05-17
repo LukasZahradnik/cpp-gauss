f268b59a83e64a52a233239d12a529382bf7d6b4
Lukáš <lukaszahradnik96@seznam.cz>
2018-05-18 01:06:48 +0200

    Prepare gauss elimination class interface

--- main.cpp
+++ main.cpp
@@ -6,3 +6,18 @@ int main()
 {
     return 0;
 }
+
+
+class GaussElimination {
+
+public:
+    static void parallel() {
+        return;
+    }
+
+public:
+    static void serial() {
+        return;
+    }
+
+};
