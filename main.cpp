dacd736039924ffaf70ac8f3eba0593b23844056
Lukáš <lukaszahradnik96@seznam.cz>
2018-05-21 13:24:14 +0200

    Implement serial gauss elimination

--- main.cpp
+++ main.cpp
@@ -3,22 +3,59 @@
 
 using namespace std;
 
-int main()
-{
-    return 0;
-}
-
+#define MATRIX_SIZE 4
 
 class GaussElimination {
 
 public:
-    static void parallel() {
+    static void parallel(double* M, double* b, const int size) {
         return;
     }
 
 public:
-    static void serial() {
+    static void serial(double* M, double* b, const int size) {
+        double* res = new double[size];
+
+        for(int i = 0; i < size - 1; i++) {
+            for(int j = i + 1; j < size; j++) {
+                double k = -M[j * size + i] / M[i * size + i];
+                b[j] = b[i] * k + b[j];
+
+                for(int l = i; l < size; l++) {
+                    M[j * size + l] = M[i * size + l] * k + M[j * size + l];
+                }
+            }
+        }
+
+        b[size - 1] /= M[size * size - 1];
+        for(int i = size - 2; i >= 0; i--) {
+            double sum = 0;
+            for(int j = i + 1; j < size; j++) {
+                sum += M[i * size + j] * b[j];
+            }
+
+            b[i] = (b[i] - sum) / M[i * size + i];
+        }
+
         return;
     }
 
 };
+
+int main()
+{
+    double* m = new double[MATRIX_SIZE * MATRIX_SIZE] {
+        0.02, 0.01, 0,   0,
+        1,    2,    1,   0,
+        0,    1,    2,   1,
+        0,    0,  100, 200
+    };
+
+    double* b = new double[MATRIX_SIZE] {
+        0.02, 1, 4, 800
+    };
+
+    GaussElimination::serial(m, b, MATRIX_SIZE);
+
+    return 0;
+}
